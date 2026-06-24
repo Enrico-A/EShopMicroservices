90bb9445b94b5309b4e4a97c1313ae97a80b2335
Enrico-A <e.arfini@gmail.com>
2026-06-24 15:50:51 +0200

    adding Catalog microservice

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter(); // Add Carter services to the DI container, which allows you to define endpoints using Carter modules.
+builder.Services.AddMediatR(config => // Register MediatR services, which allows you to implement the mediator pattern for handling requests and responses.
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly); // Register MediatR handlers from the current assembly
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter(); // Map Carter endpoints
 
 app.Run();
