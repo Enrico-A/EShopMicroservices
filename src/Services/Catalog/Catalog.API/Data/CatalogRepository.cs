4a60c36ea454a25c524f354b52beb3aa2c588feb
Enrico-A <e.arfini@gmail.com>
2026-06-30 10:33:10 +0200

    Added init database and query function

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogRepository.cs
@@ -0,0 +1,194 @@
+﻿using Catalog.API.Models;
+using NpgsqlTypes;
+
+namespace Catalog.API.Data
+{
+    public class CatalogRepository
+    {
+        private readonly NpgsqlDataSource _dataSource;
+
+        public CatalogRepository(NpgsqlDataSource dataSource)
+        {
+            _dataSource = dataSource;
+        }
+
+        /// <summary>
+        /// Gets a paginated list of products asynchronously.
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<IReadOnlyList<Product>> GetProductsAsync(int pageNumber, int pageSize, CancellationToken cancellationToken)
+        {
+            const string sql = "" +
+                "SELECT id, name, category, description, imagefile, price " +
+                "FROM products " +
+                "ORDER BY name " +
+                "OFFSET @offset " +
+                "LIMIT @limit";
+            await using var connection = await _dataSource.OpenConnectionAsync(cancellationToken);
+            await using var command = new NpgsqlCommand(sql, connection);
+            command.Parameters.AddWithValue("offset", (pageNumber - 1) * pageSize);
+            command.Parameters.AddWithValue("limit", pageSize);
+            return await ReadProductsAsync(command, cancellationToken);
+        }
+
+        /// <summary>
+        /// Gets a product by its ID asynchronously.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<Product?> GetProductByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            const string sql = "" +
+                "SELECT id, name, category, description, imagefile, price " +
+                "FROM products " +
+                "WHERE id = @id;";
+
+            await using var connection = await _dataSource.OpenConnectionAsync(cancellationToken);
+            await using var command = new NpgsqlCommand(sql, connection);
+            command.Parameters.AddWithValue("id", id);
+
+            await using var reader = await command.ExecuteReaderAsync(cancellationToken);
+            return await reader.ReadAsync(cancellationToken) ? MapProduct(reader) : null;
+        }
+
+        /// <summary>
+        /// Get products by category asynchronously.
+        /// </summary>
+        /// <param name="category"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<IReadOnlyList<Product>> GetProductsByCategoryAsync(string category, CancellationToken cancellationToken)
+        {
+            const string sql = "" +
+                "SELECT id, name, category, description, imagefile, price " +
+                "FROM products " +
+                "WHERE @category = ANY(category) " +
+                "ORDER BY name;";
+
+            await using var connection = await _dataSource.OpenConnectionAsync(cancellationToken);
+            await using var command = new NpgsqlCommand(sql, connection);
+            command.Parameters.AddWithValue("category", category);
+
+            return await ReadProductsAsync(command, cancellationToken);
+        }
+
+        /// <summary>
+        /// Creates a new product asynchronously and returns the generated product ID.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<Guid> CreateProductAsync(Product product, CancellationToken cancellationToken)
+        {
+            const string sql = "" +
+                "INSERT INTO products (id, name, category, description, imagefile, price)" +
+                "VALUES (@id, @name, @category, @description, @imageFile, @price);";
+
+            product.Id = product.Id == Guid.Empty ? Guid.NewGuid() : product.Id;
+
+            await using var connection = await _dataSource.OpenConnectionAsync(cancellationToken);
+            await using var command = new NpgsqlCommand(sql, connection);
+            AddProductParameters(command, product);
+
+            await command.ExecuteNonQueryAsync(cancellationToken);
+
+            return product.Id;
+        }
+
+        /// <summary>
+        /// Updates an existing product asynchronously and returns a boolean indicating whether the update was successful.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateProductAsync(Product product, CancellationToken cancellationToken)
+        {
+            const string sql = "" +
+                "UPDATE products " +
+                "SET name = @name, category = @category, description = @description, imagefile = @imageFile, price = @price " +
+                "WHERE id = @id;";
+
+            await using var connection = await _dataSource.OpenConnectionAsync(cancellationToken);
+            await using var command = new NpgsqlCommand(sql, connection);
+            AddProductParameters(command, product);
+
+            var affectedRows = await command.ExecuteNonQueryAsync(cancellationToken);
+            return affectedRows > 0;
+        }
+
+        /// <summary>
+        /// Deletes a product by its ID asynchronously.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task DeleteProductAsync(Guid id, CancellationToken cancellationToken)
+        {
+            const string sql = "" +
+                "DELETE FROM products " +
+                "WHERE id = @id;";
+
+            await using var connection = await _dataSource.OpenConnectionAsync(cancellationToken);
+            await using var command = new NpgsqlCommand(sql, connection);
+            command.Parameters.AddWithValue("id", id);
+
+            await command.ExecuteNonQueryAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Reads products from the database using the provided command and returns a list of Product objects.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        private static async Task<IReadOnlyList<Product>> ReadProductsAsync(NpgsqlCommand command, CancellationToken cancellationToken)
+        {
+            var products = new List<Product>();
+
+            await using var reader = await command.ExecuteReaderAsync(cancellationToken);
+            while (await reader.ReadAsync(cancellationToken))
+            {
+                products.Add(MapProduct(reader));
+            }
+
+            return products;
+        }
+
+        /// <summary>
+        /// Maps a data reader to a Product object.
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        private static Product MapProduct(NpgsqlDataReader reader)
+        {
+            return new Product
+            {
+                Id = reader.GetGuid(reader.GetOrdinal("id")),
+                Name = reader.GetString(reader.GetOrdinal("name")),
+                Category = reader.GetFieldValue<string[]>(reader.GetOrdinal("category")).ToList(),
+                Description = reader.GetString(reader.GetOrdinal("description")),
+                ImageFile = reader.GetString(reader.GetOrdinal("imagefile")),
+                Price = reader.GetDecimal(reader.GetOrdinal("price"))
+            };
+        }
+
+        /// <summary>
+        /// Adds parameters for a Product object to the provided NpgsqlCommand.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="product"></param>
+        private static void AddProductParameters(NpgsqlCommand command, Product product)
+        {
+            command.Parameters.AddWithValue("id", product.Id);
+            command.Parameters.AddWithValue("name", product.Name);
+            command.Parameters.Add("category", NpgsqlDbType.Array | NpgsqlDbType.Text).Value = product.Category.ToArray();
+            command.Parameters.AddWithValue("description", product.Description);
+            command.Parameters.AddWithValue("imageFile", product.ImageFile);
+            command.Parameters.AddWithValue("price", product.Price);
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,15 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCarter(); // Add Carter services to the DI container, which allows you to define endpoints using Carter modules.
 builder.Services.AddMediatR(config => // Register MediatR services, which allows you to implement the mediator pattern for handling requests and responses.
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly); // Register MediatR handlers from the current assembly
 });
+builder.Services.AddCarter(); // Add Carter services to the DI container, which allows you to define endpoints using Carter modules.
+builder.Services.AddSingleton(sp =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Database") // Retrieve the connection string for the database from the configuration
+    ?? throw new InvalidOperationException("Connection string 'Database' is not configured"); // and throw an exception if it's not found
+    var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString); // Create a new NpgsqlDataSourceBuilder using the connection string
+    return dataSourceBuilder.Build(); // Build and return the NpgsqlDataSource, which is used to manage database connections
+});
+builder.Services.AddScoped<CatalogInitialData>(); // Register the CatalogInitialData as a scoped service, which means a new instance will be created for each HTTP request
+builder.Services.AddScoped<CatalogRepository>(); // Register the CatalogRepository as a scoped service, which allows you to manage product data in the database
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var initialData = scope.ServiceProvider.GetRequiredService<CatalogInitialData>();
+
+    await initialData.InitializeAsync(
+        seedSampleData: app.Environment.IsDevelopment()
+    );
+}
 app.MapCarter(); // Map Carter endpoints
 
 app.Run();
